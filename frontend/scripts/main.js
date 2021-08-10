b5a4d3326e01b3c77e5a03650f9634427f4df9b1
Brent Johnson <79657494+brentleejohnson@users.noreply.github.com>
2021-08-10 16:33:27 +0200

    Delete frontend directory

--- frontend/scripts/main.js
+++
@@ -1,7 +0,0 @@
-let width = getComputedStyle(document.body).width;
-let height = getComputedStyle(document.body).height;
-
-let logContainer = document.querySelector(".login-img");
-
-logContainer.style.width = width;
-logContainer.style.height = height;
